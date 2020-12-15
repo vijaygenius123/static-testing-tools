4fa193573ebb975e2794feb254684d959a6e5a15
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-15 23:14:34 +0530

    Basic Project Setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const username = 'freddy'
+typeof username === 'strng'
+
+if(!'serviceWorker' in navigator){
+    //You have an old broweser
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
+[1, 2, 3].forEach(x => console.log(x))
